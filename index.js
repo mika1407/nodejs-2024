de39cf7ef978aca640370b91a292e0220a6c7295
mika tiihonen <koneautomaatio@hotmail.com>
2024-11-19 08:45:08 +0200

    Simple Routing

---
+++ index.js
@@ -0,0 +1,11 @@
+import getPosts, {getPostsLength} from "./postController.js";
+
+console.log(getPosts())
+console.log(`Post length: ${getPostsLength()}`)
+
+
+// const {ran, celciusToFahrenheit }= require("./utils")
+
+// console.log(`Random Numbr: ${ran()}`)
+
+// console.log(`Celcius 0 is : ${celciusToFahrenheit(0)} Fahrenheit`)
